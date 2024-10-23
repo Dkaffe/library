2d302bf0791f4784a59518b0242c0b443f72e1f6
Dkaffe <109029615+Dkaffe@users.noreply.github.com>
2024-10-23 07:36:54 +0200

    Add toggleable  <aside> form for adding books

--- main.js
+++ main.js
@@ -1,51 +1,99 @@
 const myLibrary = [];
 
-function Book(author, title, pages, read) {
+function Book(author, title, description, pages, read) {
   this.author = author;
   this.title = title;
+  this.description = description;
   this.pages = pages;
   this.read = read;
 }
 
 function addBookToLibrary() {
-  //   myLibrary.push(prompt("Add a book:"));
+  // myLibrary.push(prompt("Add a book:"));
 }
 
-const skillBook = new Book("Johnny Skill", "Skill is what it takes!", 222, false);
-const funBook = new Book("Tessa Fun", "Having Fun", 125, false);
-const cookBook = new Book("Robbie Cook", "Cooking it! 2", 12, false);
+const description = "This is just a placeholder description of a sample book to display nicely on my page!";
+
+const skillBook = new Book("Johnny Skill", "Skill is what it takes!", description, 222, false);
+const funBook = new Book("Tessa Fun", "Having Fun", description, 125, false);
+const cookBook = new Book("Robbie Cook", "Cooking it! 2", description, 12, false);
 
 myLibrary.push(skillBook), myLibrary.push(funBook), myLibrary.push(cookBook);
 
 const main = document.querySelector("main");
 
-for (let book of myLibrary) {
-  // Create the container for a card element
-  const bookCard = document.createElement("div");
-  bookCard.className = "book-card";
+function addBooks() {
+  for (let book of myLibrary) {
+    // Create the container for a card element
+    const bookCard = document.createElement("div");
+    bookCard.className = "book-card";
 
-  //   Create the title element
-  const title = document.createElement("h3");
-  title.innerText = book.title;
+    //   Create the title element
+    const title = document.createElement("h3");
+    title.innerText = book.title;
 
-  // Create the author element
-  const author = document.createElement("p");
-  author.innerText = `Author: ${book.author}`;
+    // Create the author element
+    const author = document.createElement("p");
+    author.innerText = `Author: ${book.author}`;
 
-  // Create page display element
-  const pages = document.createElement("p");
-  pages.innerText = `Total pages: ${book.pages}`;
+    // Add a short description
+    const description = document.createElement("p");
+    description.innerText = `"${book.description}"`;
 
-  // Element for reading status
-  const read = document.createElement("p");
-  read.innerText = book.read ? "You've already read this book!" : "You haven't read this book yet";
+    // Create page display element
+    const pages = document.createElement("p");
+    pages.innerText = `Total pages: ${book.pages}`;
 
-  // Append all elements to the bookCard
-  bookCard.appendChild(title);
-  bookCard.appendChild(author);
-  bookCard.appendChild(pages);
-  bookCard.appendChild(read);
+    // Element for reading status
+    const read = document.createElement("p");
+    read.innerText = book.read ? "You've already read this book!" : "You haven't read this book yet";
 
-  // Append everything to the main container
-  main.appendChild(bookCard);
+    // Append all elements to the bookCard
+    bookCard.appendChild(title);
+    bookCard.appendChild(author);
+    bookCard.appendChild(description);
+    bookCard.appendChild(pages);
+    bookCard.appendChild(read);
+
+    // Append everything to the main container
+    main.appendChild(bookCard);
+  }
 }
+
+addBooks();
+
+// Button to toggle the form to create a new book
+const newBookButton = document.querySelector(".add-book-button");
+const newBookForm = document.querySelector(".new-book-menu");
+newBookButton.addEventListener("click", () => {
+  newBookForm.classList.toggle("hidden");
+  newBookButton.innerText === "NEW BOOK"
+    ? (newBookButton.innerText = "CLOSE MENU")
+    : (newBookButton.innerText = "NEW BOOK");
+});
+
+const addBookButton = document.querySelector(".add-book");
+const bookForm = document.querySelector("form");
+addBookButton.addEventListener("click", (e) => {
+  e.preventDefault;
+
+  // Use FormData to get access to the form fields
+  const formData = new FormData(bookForm);
+
+  // Extract values
+  const title = formData.get("title");
+  const author = formData.get("author");
+  const description = formData.get("description");
+  const pages = formData.get("pages");
+  const read = formData.get("read") === "true";
+
+  // Create the new book
+  const newBook = new Book(title, author, description, pages, read);
+
+  // Add book to the library
+  myLibrary.push(newBook);
+
+  // Reset current book cards and render all new books
+  main.innerHTML = "";
+  addBooks();
+});
